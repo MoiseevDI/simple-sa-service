9b7f9825776804c38a8305e40e0c0870ee77b87e
MoiseevDI <moiseev.david@yandex.ru>
2023-04-27 13:26:18 +0300

    new model

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM python:3.6-slim
+FROM python:3.7-slim
 COPY ./ /app
 WORKDIR /app
+RUN apt update && apt install libzbar-dev git g++ -y
 RUN pip3 install -r requirements.txt && \
-    python3 -m textblob.download_corpora
+    python3 -m dostoevsky download fasttext-social-network-model
 EXPOSE 5000
 ENTRYPOINT ["python3"]
 CMD ["sentiment_analysis.py"]
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,30 +1,57 @@
-from textblob import TextBlob
 from flask import Flask, request, jsonify
 import json
 from mocked_data import MOCKED_DATA
 import requests
+from dostoevsky.tokenization import RegexTokenizer
+from dostoevsky.models import FastTextSocialNetworkModel
+
+tokenizer = RegexTokenizer()
+
+model = FastTextSocialNetworkModel(tokenizer=tokenizer)
 
 app = Flask(__name__)
 
-def analyse_sentiment(sentence):
-    polarity = TextBlob(sentence).sentences[0].polarity
-    return jsonify(
-        sentence=sentence,
-        polarity=polarity
-    )
+def normilized_sentiment(curr):
+    default = {
+        "neutral": 0,
+        "negative": 0,
+        "positive": 0,
+        "skip": 0,
+        "speech": 0,
+    }
+    default.update(curr)
+    norm = (1 - default["neutral"]) * (1 - default["skip"]) * (default["positive"] - default["negative"])
+    if norm >= 1:
+        norm = 1
+    elif norm <= -1:
+        norm = -1
+    return norm
+
+def analyse_sentiment(sentences):
+    final_result = []
+    results = model.predict(sentences, k=5)
+    for message, sentiment in zip(sentences, results):
+        final_result.append({
+            "sentence": message,
+            "raw": sentiment,
+            "polarity": normilized_sentiment(sentiment)
+        })
+    return final_result
 
 @app.route("/v1/newsanalyses", methods=['GET'])
 def news_analyses():
     raw = requests.get("https://newsdata.io/api/1/news?apikey=pub_212338a677fdfb5c0428f68e4e54e2f9e4e73&country=ru&language=ru")
     data = raw.json()["results"]
-    result = [analyse_sentiment(new["title"]) for new in data]
-    return result
+    titles = [new["title"] for new in data["results"]]
+    result = analyse_sentiment(titles)
+    return jsonify(result)
 
 @app.route("/v1alpha/newsanalyses", methods=['GET'])
 def news_mock_analyses():
     data = json.loads(MOCKED_DATA)
-    result = [analyse_sentiment(new["title"]) for new in data["results"]]
-    return result
+    titles = [new["title"] for new in data["results"]]
+    result = analyse_sentiment(titles)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
